4cb58b26f9cd82e3de7aa8f381128cf4a013ef63
Nakamura0315 <nakamurashota0315@gmail.com>
2022-11-14 04:58:33 +0700

    Init

---
+++ client.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"golang.org/x/net/websocket"
+)
+
+func main() {
+	origin := "http://localhost/"
+	url := "ws://localhost:26657/websocket"
+	ws, err := websocket.Dial(url, "", origin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var msg = make([]byte, 512)
+	var n int
+	if n, err = ws.Read(msg); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Received: %s.\n", msg[:n])
+}
