49319f9926766004b37b22ddbe06ba23bc4c3a0c
Nakamura0315 <nakamurashota0315@gmail.com>
2022-11-28 04:28:39 +0700

    Update for encrypt

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require github.com/ignite/cli v0.25.1
 
 require (
+	auction v0.0.0-00010101000000-000000000000
 	cloud.google.com/go/storage v1.27.0 // indirect
 	cosmossdk.io/errors v1.0.0-beta.7 // indirect
 	cosmossdk.io/math v1.0.0-beta.3 // indirect
@@ -101,15 +102,15 @@ require (
 	golang.org/x/crypto v0.0.0-20220924013350-4ba4fb4dd9e7 // indirect
 	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e // indirect
 	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
-	golang.org/x/net v0.1.0 // indirect
-	golang.org/x/oauth2 v0.1.0 // indirect
-	golang.org/x/sync v0.0.0-20220929204114-8fcdb60fdcc0 // indirect
-	golang.org/x/sys v0.1.0 // indirect
-	golang.org/x/term v0.1.0 // indirect
+	golang.org/x/net v0.2.0 // indirect
+	golang.org/x/oauth2 v0.2.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/term v0.2.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
-	google.golang.org/api v0.100.0 // indirect
-	google.golang.org/genproto v0.0.0-20221027153422-115e99e71e1c // indirect
+	google.golang.org/api v0.102.0 // indirect
+	google.golang.org/genproto v0.0.0-20221114212237-e4508ebdbee1 // indirect
 	google.golang.org/grpc v1.50.1 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"auction/x/auction/types"
 	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"encoding/base64"
 	"fmt"
 	"log"
 
-	// Importing the general purpose Cosmos blockchain client
 	"github.com/ignite/cli/ignite/pkg/cosmosclient"
+	// Importing the general purpose Cosmos blockchain client
 )
 
 func main() {
+	privateKey, _ := rsa.GenerateKey(rand.Reader, 30)
+
+	publicKey := privateKey.PublicKey
+
 	// Prefix to use for account addresses.
 	// The address prefix was assigned to the auction blockchain
 	// using the `--address-prefix` flag during scaffolding.
@@ -24,6 +32,14 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	accountName := "bob"
+
+	account, err := cosmos.Account(accountName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	for {
 		if cosmos.WaitForNextBlock(context.Background()) != nil {
 			continue
@@ -48,6 +64,32 @@ func main() {
 			fmt.Println(events)
 
 		}
+
+		var bidAmount = "10"
+		var encryptBidString = EncryptString(bidAmount, publicKey)
+		// broadcast keyshare message
+		broadcastMsg := &types.MsgBidAuction{
+			AuctionIndex: "1",
+			BidAmount:    encryptBidString,
+		}
+		broadcastResp, err := cosmos.BroadcastTx(context.Background(), account, broadcastMsg)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Sent keyshare: ", broadcastResp)
 	}
 
+}
+
+func EncryptString(secretMessage string,
+	key rsa.PublicKey) string {
+	rng := rand.Reader
+	ciphertext, err := rsa.EncryptPKCS1v15(rng, &key,
+		[]byte(secretMessage))
+
+	if err != nil {
+		panic(err)
+	}
+
+	return base64.StdEncoding.EncodeToString(ciphertext)
 }
